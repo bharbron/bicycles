c652f234504ccee7ed451b188ce67807d53c93f0
Brian Harbron <harbron@gmail.com>
2015-05-17 23:52:10 +0000

    Started on Bicycle_Shop class. Implemented adding bikes to inventory

--- bicycles.py
+++ bicycles.py
@@ -5,8 +5,21 @@ class Bicycle(object):
     self.cost = cost
     
 class Bicycle_Shop(object):
-  def __init__(self):
-    pass
+  def __init__(self, name, markup):
+    self.name = name
+    self.markup = markup
+    self.inventory = {}
+    self.profit = 0.0
+    
+  def add_inventory(self, bicycle, quantity):
+    if bicycle.name in self.inventory:
+      self.inventory[bicycle.name] = {'bicycle': bicycle, 'quantity': self.inventory[bicycle.name]['quantity'] + quantity}
+    else:
+      self.inventory[bicycle.name] = {'bicycle': bicycle, 'quantity': quantity}
+      
+  def print_inventory(self):
+    for name, bicycle in self.inventory.iteritems():
+      print "We have {0} bicycles of model '{1}' in stock".format(bicycle['quantity'], name)
 
 class Customer(object):
   def __init__(self):
@@ -17,3 +30,14 @@ if __name__ == '__main__':
   print roadmax.name
   print roadmax.weight
   print roadmax.cost
+  racestar = Bicycle('Racestar 20xx', 12, 480)
+  shop = Bicycle_Shop('OMG Bikes', 20)
+  print shop.name
+  print shop.markup
+  print shop.inventory
+  print shop.profit
+  shop.add_inventory(roadmax, 10)
+  shop.add_inventory(racestar, 3)
+  shop.print_inventory()
+  shop.add_inventory(racestar, 2)
+  shop.print_inventory()
