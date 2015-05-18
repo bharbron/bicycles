e8d515382e13b2fc980ed2f9824b622ba96737d9
Brian Harbron <harbron@gmail.com>
2015-05-18 00:12:32 +0000

    Implemented selling of bicycles

--- bicycles.py
+++ bicycles.py
@@ -16,14 +16,29 @@ class Bicycle_Shop(object):
       self.inventory[bicycle.name] = {'bicycle': bicycle, 'quantity': self.inventory[bicycle.name]['quantity'] + quantity}
     else:
       self.inventory[bicycle.name] = {'bicycle': bicycle, 'quantity': quantity}
+    self.profit = self.profit - (quantity * bicycle.cost)
       
   def print_inventory(self):
     for name, bicycle in self.inventory.iteritems():
       print "We have {0} bicycles of model '{1}' in stock".format(bicycle['quantity'], name)
+      
+  def print_profit(self):
+    print "We have made ${} selling bicycles".format(self.profit)
+    
+  def sell_bicycle(self, bicycle):
+    if bicycle.name in self.inventory and self.inventory[bicycle.name]['quantity'] >= 1:
+      sale_price = self.inventory[bicycle.name]['bicycle'].cost * ((self.markup / 100.0) + 1.0)
+      print "Here is your '{0}'.  That will be ${1}, please.".format(bicycle.name, sale_price)
+      self.inventory[bicycle.name]['quantity'] = self.inventory[bicycle.name]['quantity'] - 1
+      self.profit = self.profit + sale_price
+    else:
+      print "Sorry, we don't have the '{}' in stock.".format(bicycle.name)
+    self.print_inventory()
+    self.print_profit()
 
 class Customer(object):
-  def __init__(self):
-    pass
+  def __init__(self, name):
+    self.name = name
   
 if __name__ == '__main__':
   roadmax = Bicycle('Roadmax 1985', 50, 180)
@@ -36,8 +51,10 @@ if __name__ == '__main__':
   print shop.markup
   print shop.inventory
   print shop.profit
-  shop.add_inventory(roadmax, 10)
-  shop.add_inventory(racestar, 3)
+  shop.add_inventory(roadmax, 2)
+  shop.add_inventory(racestar, 1)
   shop.print_inventory()
   shop.add_inventory(racestar, 2)
   shop.print_inventory()
+  shop.print_profit()
+  shop.sell_bicycle(roadmax)
