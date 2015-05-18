318b12c0414ad8c367812c917babdc5b0a2fa107
Brian Harbron <harbron@gmail.com>
2015-05-18 12:32:50 +0000

    Implemented searching by price; Added helper functions to Store; cleaned up formatting on print functions

--- bicycles.py
+++ bicycles.py
@@ -4,6 +4,9 @@ class Bicycle(object):
     self.weight = weight
     self.cost = cost
     
+  def __repr__(self):
+    return 'Bicycle("{0}","{1}","{2:.2f}")'.format(self.name, self.weight, self.cost)
+    
 class Bicycle_Shop(object):
   def __init__(self, name, markup):
     self.name = name
@@ -19,34 +22,49 @@ class Bicycle_Shop(object):
     self.profit = self.profit - (quantity * bicycle.cost)
       
   def print_inventory(self):
-    for name, bicycle in self.inventory.iteritems():
-      print "We have {0} bicycles of model '{1}' in stock".format(bicycle['quantity'], name)
+    for name, item in self.inventory.iteritems():
+      print "We have {0} bicycles of model '{1}' in stock".format(item['quantity'], name)
       
   def print_profit(self):
-    print "We have made ${} selling bicycles".format(self.profit)
+    print "We have made ${:.2f} selling bicycles".format(self.profit)
+    
+  def calc_sale_price(self, bicycle):
+    return bicycle.cost * ((self.markup / 100.0) + 1.0)
     
   def sell_bicycle(self, bicycle):
     if bicycle.name in self.inventory and self.inventory[bicycle.name]['quantity'] >= 1:
-      sale_price = self.inventory[bicycle.name]['bicycle'].cost * ((self.markup / 100.0) + 1.0)
-      print "Here is your '{0}'.  That will be ${1}, please.".format(bicycle.name, sale_price)
+      sale_price = self.calc_sale_price(self.inventory[bicycle.name]['bicycle'])
+      print "Here is your '{0}'.  That will be ${1:.2f}, please.".format(bicycle.name, sale_price)
       self.inventory[bicycle.name]['quantity'] = self.inventory[bicycle.name]['quantity'] - 1
       self.profit = self.profit + sale_price
     else:
       print "Sorry, we don't have the '{}' in stock.".format(bicycle.name)
-    self.print_inventory()
-    self.print_profit()
+      
+  def search_by_price(self, fund):
+    affordable_bikes = []
+    for name, item in self.inventory.iteritems():
+      sale_price = self.calc_sale_price(item['bicycle'])
+      if item['quantity'] >= 1 and sale_price <= fund:
+        affordable_bikes.append(item['bicycle'])
+    return affordable_bikes
+      
 
 class Customer(object):
   def __init__(self, name, fund):
     self.name = name
     self.fund = fund
+    self.bicycle = None
+    
+  def buy_bicycle(self):
+    pass
   
 if __name__ == '__main__':
-  roadmax = Bicycle('Roadmax 1985', 50, 180)
+  roadmax = Bicycle('Roadmax 1985', 50, 100)
   print roadmax.name
   print roadmax.weight
   print roadmax.cost
-  racestar = Bicycle('Racestar 20xx', 12, 480)
+  print roadmax
+  racestar = Bicycle('Racestar 20xx', 12, 400)
   shop = Bicycle_Shop('OMG Bikes', 20)
   print shop.name
   print shop.markup
@@ -59,3 +77,9 @@ if __name__ == '__main__':
   shop.print_inventory()
   shop.print_profit()
   shop.sell_bicycle(roadmax)
+  shop.print_inventory()
+  shop.print_profit()
+  bob = Customer('Bob', 200.0)
+  print bob.name
+  print bob.fund
+  print shop.search_by_price(bob.fund)
